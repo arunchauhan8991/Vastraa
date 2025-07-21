f5a824aa4348b0b2e32cbab69084868d130667a0
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-07-21 16:13:52 +0530

    add routing and registraion and login page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from 'react'
+import Registration from './pages/Registration.jsx'
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
 
 function App() {
-
   return (
     <>
-      
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/signup" element={<Registration/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
     </>
   )
 }
