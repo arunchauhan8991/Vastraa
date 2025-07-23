645ca88453402766873ec9d60362e08843afc4aa
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-07-23 16:09:48 +0530

    admin vite setup

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: ["http://localhost:5174", "http://localhost:5175"],
+    credentials: true,
   })
 );
 app.use("/api/auth", authRoutes)
